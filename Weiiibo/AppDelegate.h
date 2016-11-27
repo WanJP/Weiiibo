bc4395695357e14453af91f9d0447677de23c547
WanJP <782966008@qq.com>
2016-11-27 15:47:56 +0800

    Project initialization

---
+++ Weiiibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Weiiibo
+//
+//  Created by 万贾平 on 16/11/27.
+//  Copyright © 2016年 jpw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
